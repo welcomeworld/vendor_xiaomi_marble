a495bde852352c89080f8311fa8ba88863b3f449
welcomeworld <guduziyousiwang@gmail.com>
2026-04-11 09:58:48 +0800

    init

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),marble)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,7991eaa0d9e8e87d8f560bc5517dda0274ddf958)
+$(call add-radio-file-sha1-checked,radio/aop.img,42a55d4855821737fba4c41fd410ac3f5586fc6e)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,664556610836c8074237354bed6c33de2d8ad3b2)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,da3428d3108086c2de3bce614213ea179bf7e369)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,5d79493c9d74757b4f7e9f4cd451193de69e3c65)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,15c26dde4e5d1059ee6bc6cc24f7282c7d9b24d3)
+$(call add-radio-file-sha1-checked,radio/dsp.img,abb0cf432db0d513601b5314b55b1765dc900317)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,9f994fc23a276cc27dc2870bc1429de13b1d1bb8)
+$(call add-radio-file-sha1-checked,radio/hyp.img,a2ce60e05715618f719c01b6f8db0f6bd44ec796)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,b457bbead4419f870111dfb40ca9bb3f5e0fcd58)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,9db053ba77db79b4eff3618ac511e7edb56ce17b)
+$(call add-radio-file-sha1-checked,radio/modem.img,798826b6e5652f1b32e27b544a9694e6c248e9b9)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,a79e944cc9f3e87a4cb263e3be774b852df9bf43)
+$(call add-radio-file-sha1-checked,radio/shrm.img,19579327ebfdc55b57f4c1b058d0c82150061aeb)
+$(call add-radio-file-sha1-checked,radio/tz.img,f51478b1e64789cb07f8f1c68f05ba3c97890423)
+$(call add-radio-file-sha1-checked,radio/uefi.img,aeffa65d8869d26b0a1807bd8080768732c361a0)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,7de6421928c4be404c9a3593d650bd18ce8791eb)
+$(call add-radio-file-sha1-checked,radio/xbl.img,da5c6fd844bc4870f258d400ac2906b44b222f38)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,0b7569af063ea1ca26bff1402ced92562d8c85a4)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,83011e4aa2addcc32a2b9c552ab21be70ce8264e)
+
+endif
